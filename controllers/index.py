3f90586a6da79e7f3d9a28c70aa0d7d69d6c6cce
phx <945871257@qq.com>
2021-01-29 23:33:02 +0800

    modify blog function

--- controllers/index.py
+++ controllers/index.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+from models.blog import Blog
 
 index = Blueprint('index', __name__)
 
 
 @index.route('/')
 def indexpage():
-    return render_template('index.html')
+    blog_instance = Blog()
+    blogs = blog_instance.search_blog_by_limit(0, 4)
+    return render_template('index.html', blogs=blogs)
--- models/blog.py
+++ models/blog.py
@@ -10,3 +10,15 @@ db_session, db_model, db_metadata = init_db()
 class Blog(db_model):
     __tablename__ = 'blog'
     __table__ = Table(__tablename__, db_metadata, autoload=True)
+
+    def find_all(self):
+        result = db_session.query(Blog).all()
+        return result
+
+    def search_blog_by_id(self, id):
+        result = db_session.query(Blog).filter_by(id=id).all()
+        return result
+
+    def search_blog_by_limit(self, start, count):
+        result = db_session.query(Blog).order_by(Blog.updatetime.desc()).limit(count).offset(start)
+        return result
