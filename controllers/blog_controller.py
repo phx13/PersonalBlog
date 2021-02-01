e503418d408fa3a14feee0b9b66ebd96bb06b917
phx <945871257@qq.com>
2021-02-01 23:43:08 +0800

    deal with template inheritance

--- controllers/blog_controller.py
+++ controllers/blog_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from models.blog_model import BlogModel
+import math
 
 blog_blueprint = Blueprint('blog_blueprint', __name__)
 
@@ -7,5 +8,37 @@ blog_blueprint = Blueprint('blog_blueprint', __name__)
 @blog_blueprint.route('/blog')
 def blog_page():
     blog_model = BlogModel()
-    blog_page_blogs = blog_model.search_blog_by_limit(0, 10)
-    return render_template('blog.html', blog_page_blogs=blog_page_blogs)
+    blogs_per_page = 6
+    current_page_blogs = blog_model.search_blog_by_limit(0, blogs_per_page)
+    total_page = math.ceil(blog_model.search_blog_total_count() / blogs_per_page)
+    return render_template('blog.html', current_page_blogs=current_page_blogs, current_page=1, total_page=total_page)
+
+
+@blog_blueprint.route('/blog/<int:current_page>')
+def blog_current_page(current_page):
+    blog_model = BlogModel()
+    blogs_per_page = 6
+    offset_count = (current_page - 1) * blogs_per_page
+    current_page_blogs = blog_model.search_blog_by_limit(offset_count, blogs_per_page)
+    total_page = math.ceil(blog_model.search_blog_total_count() / blogs_per_page)
+    return render_template('blog.html', current_page_blogs=current_page_blogs, current_page=current_page, total_page=total_page)
+
+
+@blog_blueprint.route('/blog-search/<int:current_page>-<keyword>')
+def blog_current_page_by_search(current_page, keyword):
+    blog_model = BlogModel()
+    blogs_per_page = 6
+    offset_count = (current_page - 1) * blogs_per_page
+    current_page_blogs = blog_model.search_blog_by_query_article(keyword, offset_count, blogs_per_page)
+    total_page = math.ceil(blog_model.search_blog_total_count_by_article(keyword) / blogs_per_page)
+    return render_template('blog_search.html', current_page_blogs=current_page_blogs, current_page=current_page, total_page=total_page, keyword=keyword)
+
+
+@blog_blueprint.route('/blog-type/<int:current_page>-<type>')
+def blog_current_page_by_type(current_page, type):
+    blog_model = BlogModel()
+    blogs_per_page = 6
+    offset_count = (current_page - 1) * blogs_per_page
+    current_page_blogs = blog_model.search_blog_by_type(type, offset_count, blogs_per_page)
+    total_page = math.ceil(blog_model.search_blog_total_count_by_type(type) / blogs_per_page)
+    return render_template('blog_type.html', current_page_blogs=current_page_blogs, current_page=current_page, total_page=total_page, type=type)
--- models/blog_model.py
+++ models/blog_model.py
@@ -9,14 +9,42 @@ class BlogModel(db_model):
     __tablename__ = 'blog'
     __table__ = Table(__tablename__, db_metadata, autoload=True)
 
+    # 查询所有博客
     def search_all_blog(self):
         result = db_session.query(BlogModel).all()
         return result
 
+    # 查询文章总数量
+    def search_blog_total_count(self):
+        result = db_session.query(BlogModel).count()
+        return result
+
+    # 根据id查询博客
     def search_blog_by_id(self, id):
         result = db_session.query(BlogModel).filter_by(id=id).all()
         return result
 
+    # 查询指定数量的博客
     def search_blog_by_limit(self, start, count):
         result = db_session.query(BlogModel).order_by(BlogModel.updatetime.desc()).limit(count).offset(start)
+        return result
+
+    # 根据模糊搜索标题查询指定数量的博客
+    def search_blog_by_query_article(self, keyword, start, count):
+        result = db_session.query(BlogModel).filter(BlogModel.article.like('%' + keyword + '%')).order_by(BlogModel.updatetime.desc()).limit(count).offset(start)
+        return result
+
+    # 根据模糊查询标题查询文章总数量
+    def search_blog_total_count_by_article(self, keyword):
+        result = db_session.query(BlogModel).filter(BlogModel.article.like('%' + keyword + '%')).count()
+        return result
+
+    # 根据类别查询指定数量的博客
+    def search_blog_by_type(self, type, start, count):
+        result = db_session.query(BlogModel).filter(BlogModel.type.like('%' + type + '%')).order_by(BlogModel.updatetime.desc()).limit(count).offset(start)
+        return result
+
+    # 根据类别查询文章总数量
+    def search_blog_total_count_by_type(self, type):
+        result = db_session.query(BlogModel).filter(BlogModel.type.like('%' + type + '%')).count()
         return result
