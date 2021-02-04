7fc7a058eabb1654ff418ce4f63ac3588f4d98ef
phx <945871257@qq.com>
2021-02-04 12:18:21 +0800

    test remote env

--- commons/db_orm_helper.py
+++ commons/db_orm_helper.py
@@ -2,7 +2,7 @@ from sqlalchemy import MetaData
 
 
 def init_db():
-    from main import db
+    from app import db
     db_session = db.session
     db_model = db.Model
     db_metadata = MetaData(bind=db.engine)
